edb4525912bf05e5b05f6d1d936ded42f66c3f97
Jovany Hochart <jovanyhochart@gmail.com>
2025-08-07 15:04:52 +0200

    whole header with navbar + logo + phone number button done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import Navbar from './Navbar';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../assets/img/DEPANAUTO62_Logo.jpg';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+    const [showPhoneNumber, setShowPhoneNumber] = useState(false);
+    
+    //function to change the content of the phone number button when the user clicks on it
+    const togglePhoneButton = () => {
+        setShowPhoneNumber(prev => !prev);
+    }
+
     return (
-        <div>
-            Ceci est un header !
-        </div>
+        <header className="header-container">
+            <NavLink to="/" content="" className="header-logo">
+                <img src={logo} alt="Logo DEPANAUTO62" className="header-logo-img"></img>
+                <div className="header-logo-text">
+                    <h1 className="header-logo-title"><span>-</span>DEPAN AUTO 62<span>-</span></h1>
+                    <h4 className="header-logo-slogan">VOTRE MÉCANICIEN À DOMICILE</h4>
+                </div>
+            </NavLink>
+
+            <Navbar className="header-navbar"/>
+            <button onClick={togglePhoneButton} className="header-phone-btn">
+                <FontAwesomeIcon className="phone-icon" icon={faPhone} />
+                {showPhoneNumber ? ' 07 49 70 57 35' : ' Appelez-nous'}
+            </button>
+        </header>
     );
 }
 
