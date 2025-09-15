923e014424004d74512b2859020958627d4393a3
Jovany Hochart <jovanyhochart@gmail.com>
2025-09-15 12:18:41 +0200

    banner for photos gallery

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -8,6 +8,8 @@ function Photos() {
     const [images, setImages] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    useRevealOnScroll(".fade-in-up, .fade-in-side-left, .fade-in-side-right");
+
     useEffect(() => {
         document.title = "DEPAN AUTO 62 - Galerie photos";
 
@@ -33,8 +35,14 @@ function Photos() {
 
     return (
         <div className="photos-container">
-            <h1 className="photos-title">Galerie photos</h1>
-
+            {/* Banner */}
+            <div className="photos-banner">
+                <img src="/photos_depan_auto_62/depan_auto_62_galerie_photos.jpg" alt="Prestations DEPAN AUTO 62" className="photos-banner-img"/>
+                <div className="photos-banner-title fade-in-up">
+                    <h1>Découvrez votre dépanneur en images !</h1>
+                </div>
+            </div>
+            {/* Main content */}
             <div className="photos-gallery-container">
                 {loading && <div className="photos-gallery-loader"></div>}
                 {!loading && images.length === 0 && <p className="photos-gallery-notFound">Aucune photo n'a été trouvée.</p>}
