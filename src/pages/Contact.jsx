5f1fc7193ddb47e5d1894d2887534e17e1109b2f
Jovany Hochart <jovanyhochart@gmail.com>
2025-10-06 15:37:54 +0200

    comment for future contact setup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -70,7 +70,7 @@ function Contact() {
                     <h1 className="contact-form-title">Prendre rendez-vous</h1>
                     <form
                         onSubmit={handleSubmit}
-                        action="https://formsubmit.co/6fb952b90f78235eea697297767b28be"
+                        action="https://formsubmit.co/6fb952b90f78235eea697297767b28be" //<form action="https://formsubmit.co/your@email.com" method="POST">
                         method="POST"
                         className="contact-form"
                     >
