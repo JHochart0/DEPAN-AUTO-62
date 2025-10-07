e230ab69145bc01f13e16dde8db80e0eb6e00f38
Jovany Hochart <jovanyhochart@gmail.com>
2025-10-07 14:55:51 +0200

    changed back the mail in contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -84,8 +84,7 @@ function Contact() {
                     <h1 className="contact-form-title">Prendre rendez-vous</h1>
                     <form
                         onSubmit={handleSubmit}
-                        action="https://formsubmit.co/jovanyhochart@gmail.com"
-                        //action="https://formsubmit.co/ce5f3d1e4bc9a23382b07adf7b3b23ed"
+                        action="https://formsubmit.co/ce5f3d1e4bc9a23382b07adf7b3b23ed"
                         method="POST"
                         className="contact-form"
                     >
